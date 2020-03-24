a6395f59f9d8885fe1fe0e8d4c318e0a09e583d9
Deborah  Ajayi <54291723+speak2debby@users.noreply.github.com>
2020-03-24 21:01:23 +0100

    JS assignment

---
+++ indez.js
@@ -0,0 +1,20 @@
+// Store your name in a varibale.
+
+// Store your courses in an array.
+
+// Display your name and courses and do one of the following:
+
+
+// 1. If the number of courses you are doing is an odd number, find and display all odd numbers from 1 - 200(inclusive)
+// 2. If the number of courses you are doing is an even number, find and display all even numbers from 1 - 200(inclusive)
+
+let name= "Ajayi Deborah";
+let TrackCourse= ['html','css', 'js'];
+console.log(name);
+console.log(TrackCourse);
+for (let i = 1; i <= 200; i+=2) {
+console.log(i);
+}
+
+
+
